138570cd0870ed59ab9beca8117dcd237f50a7dc
Koji Ikeda <konge0310@icloud.com>
2019-06-28 13:36:04 +0900

    incre_end

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,8 +4,9 @@ function appendUser(user) {
   var html = `<div class="chat-group-user clearfix">
   <p class="chat-group-user__name">${user.name}</p>
   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
-</div>`
-
+  
+  
+  </div>`
             $("#user-search-result").append(html);
 }
 
@@ -17,6 +18,8 @@ function appendErrMsgToHTML(message) {
 
             $("#user-search-result").append(html);
 }
+
+
 
 
 $(function() {
@@ -41,5 +44,50 @@ $(function() {
         appendErrMsgToHTML("一致するユーザーが見つかりません");
       }
     })
+    .fail(function() {
+      alert('名前検索に失敗しました');
+    })
   });
 });
+
+$(function() {
+  $(document).on("click",".user-search-add",function(){
+    var input = $(".chat-group-users").val();
+    adduserid = $(this).data('user-id')
+    addusername = $(this).data('user-name')
+    appendAddUser(adduserid, addusername);
+    $(this).parent().remove();
+  })
+});
+
+function appendAddUser(adduserid, addusername) {
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+  <input name='group[user_ids][]' type='hidden' value='${adduserid}'>
+  <p class='chat-group-user__name'>${addusername}</p>
+  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+</div>`
+       $(".chat-group-users").append(html);
+
+    }
+
+    $(document).on("click",".user-search-remove",function(){
+      console.log("発火")
+      $(this).parent().remove();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // var html = `<div class='chat-group-users' data-id="${adduserid}">
+  // ${addusername}
+  //             </div>`
+  //         $(".chat-group-users").append(html);
