d7d33dca64c35bedab5a09e1e9b432d62a8bf3e0
Koji Ikeda <konge0310@icloud.com>
2019-07-03 18:48:10 +0900

    up

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
+  process resize_to_fit: [800, 800]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :fog
@@ -12,8 +13,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  process resize_to_fit: [800, 800]
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
